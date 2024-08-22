9e48d8022430af90780370a0cf73ae6651ee31fb
rusokolov <rusokolov4@gmail.com>
2024-08-22 23:01:24 +0300

    add lesson-12

--- src/main/java/org/example/lesson_12/Main.java
+++ src/main/java/org/example/lesson_12/Main.java
@@ -12,35 +12,13 @@ public class Main {
             //String[][] matrix = {{"1", "2", "3", "34"}, {"0", "9", "6", "1"}, {"7", "8", "9", "0"}, {"7", "8", "9", ""}};
             /* Верный формат */
             String[][] matrix = {{"1", "1", "-1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
-            int sum = getSum(matrix);
+            Total total = new Total(matrix);
+            int sum = total.getSum();
             System.out.println("Сумма всех элементов матрицы = " + sum);
         } catch (MyArraySizeException e) {
             System.out.println(e.getMessage());
         } catch (MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static int getSum(String[][] matrix) throws MyArraySizeException, MyArrayDataException {
-        int total = 0;
-        if (matrix.length != 4 || matrix[0].length != 4) {
-            throw new MyArraySizeException("Неверный размер матрицы!");
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (!isNumber(matrix[i][j])) {
-                    throw new MyArrayDataException(i, j);
-                }
-                total += Integer.parseInt(matrix[i][j]);
-            }
-        }
-        return total;
-    }
-
-    public static boolean isNumber(String element) {
-        if (element.isEmpty() || !element.matches("^-?\\d+")) {
-            return false;
-        }
-        return true;
     }
 }
