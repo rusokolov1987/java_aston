a460138d2be4dba2ff7b73762b3324f0d8ce0ec2
rusokolov <rusokolov4@gmail.com>
2024-08-29 00:34:13 +0300

    add test junit

---
+++ src/main/java/org/example/lesson_14_junit/Factorial.java
@@ -0,0 +1,18 @@
+package org.example.lesson_14_junit;
+
+import org.example.lesson_14_junit.exception.InvalidValue;
+
+public class Factorial {
+    public int getFactorial(String number) throws InvalidValue {
+        if (number == null || number.isEmpty() || !number.matches("^-?\\d+")) {
+            throw new InvalidValue("Вы ввели не целое число!");
+        }
+        if (Integer.parseInt(number) < 0) {
+            throw new InvalidValue("Вы ввели не положительное целое число!");
+        }
+        if (Integer.parseInt(number) == 0) {
+            return 1;
+        }
+        return Integer.parseInt(number) * getFactorial(String.valueOf(Integer.parseInt(number) - 1));
+    }
+}
---
+++ src/main/java/org/example/lesson_14_junit/Main.java
@@ -0,0 +1,19 @@
+package org.example.lesson_14_junit;
+
+import org.example.lesson_14_junit.exception.InvalidValue;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        try {
+            System.out.println("Введите положительное целое число.");
+            Scanner scanner = new Scanner(System.in);
+            String number = scanner.nextLine();
+            System.out.println(factorial.getFactorial(number));
+        } catch (InvalidValue e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
